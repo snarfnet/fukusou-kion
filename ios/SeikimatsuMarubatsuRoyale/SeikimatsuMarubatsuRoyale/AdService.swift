01bf610d98c2d69bdd83fc2f447a1e835bd24ca7
snarfnet <82538502+snarfnet@users.noreply.github.com>
2026-05-18 06:11:46 +0900

    Disable ad SDK launch path

--- ios/SeikimatsuMarubatsuRoyale/SeikimatsuMarubatsuRoyale/AdService.swift
+++ ios/SeikimatsuMarubatsuRoyale/SeikimatsuMarubatsuRoyale/AdService.swift
@@ -1,85 +1,17 @@
-import GoogleMobileAds
 import SwiftUI
-import UIKit
 
 @MainActor
-final class RewardedAdService: NSObject, ObservableObject, GADFullScreenContentDelegate {
+final class RewardedAdService: ObservableObject {
     static let shared = RewardedAdService()
 
     @Published private(set) var isLoaded = false
     @Published private(set) var isLoading = false
 
-    private let rewardedAdUnitID = "ca-app-pub-9404799280370656/8535760863"
-    private var rewardedAd: GADRewardedAd?
-    private var pendingReward: (() -> Void)?
+    private init() {}
 
-    private override init() {
-        super.init()
-    }
-
-    func load() {
-        guard !isLoading else { return }
-
-        isLoading = true
-        Task { @MainActor in
-            do {
-                let ad = try await GADRewardedAd.load(
-                    withAdUnitID: rewardedAdUnitID,
-                    request: GADRequest()
-                )
-                ad.fullScreenContentDelegate = self
-                rewardedAd = ad
-                isLoaded = true
-            } catch {
-                rewardedAd = nil
-                isLoaded = false
-            }
-            isLoading = false
-        }
-    }
+    func load() {}
 
     func showRewardedAd(onReward: @escaping () -> Void, onUnavailable: @escaping () -> Void) {
-        guard let rewardedAd,
-              let rootViewController = UIApplication.shared.adRootViewController else {
-            onUnavailable()
-            load()
-            return
-        }
-
-        self.rewardedAd = nil
-        isLoaded = false
-        pendingReward = onReward
-
-        rewardedAd.present(fromRootViewController: rootViewController) { [weak self] in
-            self?.pendingReward?()
-            self?.pendingReward = nil
-        }
-    }
-
-    func adDidDismissFullScreenContent(_ ad: GADFullScreenPresentingAd) {
-        pendingReward = nil
-        load()
-    }
-
-    func ad(_ ad: GADFullScreenPresentingAd, didFailToPresentFullScreenContentWithError error: Error) {
-        pendingReward = nil
-        load()
-    }
-}
-
-private extension UIApplication {
-    var adRootViewController: UIViewController? {
-        connectedScenes
-            .compactMap { $0 as? UIWindowScene }
-            .flatMap(\.windows)
-            .first { $0.isKeyWindow }?
-            .rootViewController?
-            .topPresentedViewController
-    }
-}
-
-private extension UIViewController {
-    var topPresentedViewController: UIViewController {
-        presentedViewController?.topPresentedViewController ?? self
+        onUnavailable()
     }
 }
--- ios/SeikimatsuMarubatsuRoyale/SeikimatsuMarubatsuRoyale/SeikimatsuMarubatsuRoyaleApp.swift
+++ ios/SeikimatsuMarubatsuRoyale/SeikimatsuMarubatsuRoyale/SeikimatsuMarubatsuRoyaleApp.swift
@@ -1,23 +1,15 @@
 import SwiftUI
-import GoogleMobileAds
 
 @main
 struct SeikimatsuMarubatsuRoyaleApp: App {
     @StateObject private var game = GameStore()
     @StateObject private var adService = RewardedAdService.shared
 
-    init() {
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
-    }
-
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(game)
                 .environmentObject(adService)
-                .task {
-                    adService.load()
-                }
         }
     }
 }
