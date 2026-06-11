5fa1ff1d431ccb1c053fff22cd011540fca91c83
snarfnet <82538502+snarfnet@users.noreply.github.com>
2026-06-11 19:45:36 +0900

    Wait for current Voiceprint upload

--- ios/VoiceprintInstallation/scripts/wait_for_testflight_build.py
+++ ios/VoiceprintInstallation/scripts/wait_for_testflight_build.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import time
+from datetime import datetime
 
 from asc_helpers import api_json, fail
 
@@ -9,11 +10,20 @@ APP_ID = os.environ["APP_ID"]
 BUILD_NUMBER = os.environ["BUILD_NUMBER"]
 WAIT_SECONDS = int(os.environ.get("WAIT_SECONDS", "1800"))
 POLL_SECONDS = int(os.environ.get("POLL_SECONDS", "30"))
+MIN_UPLOADED_AT = os.environ.get("UPLOAD_STARTED_AT", "")
 
 
 def find_builds():
     builds = api_json("GET", f"/apps/{APP_ID}/builds?limit=10").get("data", [])
-    return sorted(builds, key=lambda item: item.get("attributes", {}).get("uploadedDate") or "", reverse=True)
+    builds = sorted(builds, key=lambda item: item.get("attributes", {}).get("uploadedDate") or "", reverse=True)
+    if MIN_UPLOADED_AT:
+        min_dt = datetime.fromisoformat(MIN_UPLOADED_AT.replace("Z", "+00:00"))
+        builds = [
+            item for item in builds
+            if item.get("attributes", {}).get("uploadedDate")
+            and datetime.fromisoformat(item["attributes"]["uploadedDate"]).astimezone(min_dt.tzinfo) >= min_dt
+        ]
+    return builds
 
 
 def main():
